5f25393776eb27e719335e41a3b0a80344270999
hominhduc18 <hoduc092@gmail.com>
2023-09-22 18:10:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chatapp/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.chatapp
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "AAAApJodn8s:APA91bHXqj0no_9RrxzJ7f_3b_X-0OwJshHo_dPSYJVRl4VP2m4P1cTM9LkN6759NS2aAKIdB4hLM4EKT-wojwzc2pnfnSpHEZssRPt3oMaFoiHpIcj6YO83eSxhFstxNlbpPNuSJev8"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
